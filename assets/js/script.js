d2fb2149808ea32b3b52333724f63b901a04ba38
Jsieler <joshuasieler@Joshs-MacBook-Air.local>
2021-08-14 19:33:07 -0600

    added  form to input city name search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,75 @@
 var apiKey = `e59237408fbdfe54dda7ee71ee1dabd9`
-var defaultCity = `Salt Lake City`
 var defaultUnits = `&units=imperial`
 
-var currentWeatherEl=document.querySelector("#current-weather");
 
-fetch (`https://api.openweathermap.org/data/2.5/onecall?lat=40.7608&lon=-111.8911${defaultUnits}&appid=${apiKey}`
-  )   .then(function(response) {
-      return response.json();
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city-input");
+var cityWeatherSearch = document.querySelector("#search-city-weather");
+var cityUvi = document.querySelector(".city-uvi");
+
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    var city = cityInputEl.value.trim();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}${defaultUnits}&appid=${apiKey}`
+    ).then(function (response) {
+        return response.json();
+
     })
-    .then(function(response) {
-      console.log(response);
-      
-      var temp = document.createElement("p")
-      temp.textContent = "Temp:" + response.current.temp
+        .then(function (response, lat, lon) {
+            var lon = response.coord.lon
+            var lat = response.coord.lat
+            console.log(lon, lat)
+            console.log(response);
+            if (city) {
+                cityInputEl.value = "";
 
-      var windSpeed = document.createElement("p")
-      windSpeed.textContent = "Wind Speed:" + response.current.wind_speed + 'MPH'
+                var displayCurrentDate = document.querySelector(".city-current-date");
+                var currentDate = moment();
+                displayCurrentDate.textContent = currentDate.format("(L)");
 
-      var humidity = document.createElement("p")
-      humidity.textContent = "Humidity:" + response.current.humidity + '%'
+                var currentDate = moment();
+                console.log(currentDate.format())
 
-      var uvi = document.createElement("p")
-      uvi.textContent = "UVI Index:" + response.current.uvi
+                var cityName = document.createElement("h2")
+                cityName.textContent = response.name
 
-      currentWeatherEl.append(temp,windSpeed,humidity,uvi)
-    });
+                var temp = document.createElement("p")
+                temp.textContent = " Temp: " + response.main.temp + ' °F'
+
+                var windSpeed = document.createElement("p")
+                windSpeed.textContent = " Wind Speed: " + response.wind.speed + ' MPH'
+
+                var humidity = document.createElement("p")
+                humidity.textContent = " Humidity: " + response.main.humidity + ' %'
+
+
+                cityWeatherSearch.append(cityName, temp, windSpeed, humidity)
+
+                fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}${defaultUnits}&appid=${apiKey}`
+                ).then(function (response) {
+                    return response.json();
+                })
+                    .then(function (response) {
+                        console.log(response);
+
+                        var uvi = document.createElement("p")
+                        uvi.textContent = " UV Index: " + response.current.uvi
+
+
+                        cityUvi.append(uvi)
+                    });
+            }
+        });
+
+};
+
+
+// search button 
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+
+   
+
+   
